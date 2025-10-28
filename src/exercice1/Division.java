262f55e64ae24f4a4ee2779fe5705d109417f7d7
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-10-28 13:46:48 +0000

    fix division

---
+++ src/exercice1/Division.java
@@ -0,0 +1,27 @@
+package exercice1;
+
+import java.util.Scanner;
+
+public class Division {
+    static int[] tableau = {17, 12, 15, 38, 29, 157, 89, -22, 0, 5};
+
+    static int division (int indice,int diviseur) {
+        if (diviseur == 0){
+            throw new ArithmeticException("cannot Divide By 0 : My exception");
+        }
+            return tableau[indice]/diviseur;
+    }
+
+        public static void main (String[] args) {
+            int x, y;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Entrez l’indice de l’entier à diviser: ");
+            x = sc.nextInt();
+            System.out.println ("Entrez le diviseur: ");
+            y = sc.nextInt();
+            sc.close();
+            System.out.println ("Le résultat de la division est: ");
+            System.out.println (division(x,y));
+    }
+ }
+
